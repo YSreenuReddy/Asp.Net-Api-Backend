3db7355b1abfc66e0e953168bfb1cefb5afff065
YSreenuReddy <srereddy8@gmail.com>
2026-06-19 13:56:14 +0530

    i add the azure db for that i made changes.

--- StudentCoreEx1/Program.cs
+++ StudentCoreEx1/Program.cs
@@ -4,14 +4,14 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Railway PORT support
-//var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-//builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 builder.Services.AddControllers();
 
-//builder.Services.AddDbContext<StudentDbContext>(options =>
-//    options.UseSqlServer(
-//        builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<StudentDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

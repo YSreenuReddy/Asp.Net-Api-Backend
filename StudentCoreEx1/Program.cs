80d43ea462f06650a836b27da6f352b1f4746fcb
YSreenuReddy <srereddy8@gmail.com>
2026-06-19 15:42:48 +0530

    fix railway startup.

--- StudentCoreEx1/Program.cs
+++ StudentCoreEx1/Program.cs
@@ -4,15 +4,17 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Railway PORT
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+var port = Environment.GetEnvironmentVariable("PORT");
+if (!string.IsNullOrEmpty(port))
+{
+    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+}
 
 builder.Services.AddControllers();
 
-// REMOVE DB TEMPORARILY
-// builder.Services.AddDbContext<StudentDbContext>(options =>
-// options.UseSqlServer(
-// builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<StudentDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -36,7 +38,8 @@ app.UseCors("AllowAngular");
 
 app.UseAuthorization();
 
-// TEMP TEST ROUTE
+// KEEP BOTH
 app.MapGet("/", () => "API Running");
+app.MapControllers();
 
 app.Run();

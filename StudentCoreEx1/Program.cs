a66f3f23bd2817768d208f729e2d4910cad51652
YSreenuReddy <srereddy8@gmail.com>
2026-06-19 11:50:54 +0530

    changes made in program.cs and controller.cs

--- StudentCoreEx1/Program.cs
+++ StudentCoreEx1/Program.cs
@@ -4,8 +4,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Railway PORT support
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+//var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+//builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 builder.Services.AddControllers();
 
